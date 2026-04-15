edd90857ac6727dc2827762e70945289806e6980
nogueiralegacy <me@nogueiralegacy.com>
2026-04-15 15:17:39 -0300

    chore: consolida migrations V1/V2/V3 em um único script inicial

--- src/main/resources/db/migration/V3__add_content_mode_to_code_systems.sql
+++
@@ -1,7 +0,0 @@
--- Persiste o CodeSystem.content (complete, fragment, supplement, not-present, example)
--- para que o loader possa diferenciar merges legítimos de fragments de colisões reais.
-ALTER TABLE terminology.code_systems
-    ADD COLUMN content_mode VARCHAR(32);
-
-COMMENT ON COLUMN terminology.code_systems.content_mode IS
-    'Valor do campo FHIR CodeSystem.content (CodeSystemContentMode)';
